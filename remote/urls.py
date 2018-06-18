c1fd0f26ec389987248f52e9671ed6cf6b2a4979
SimenHolmestad <simen@holmestad.com>
2018-06-18 12:34:39 +0000

    Made so you can access photos by album

--- remote/urls.py
+++ remote/urls.py
@@ -5,6 +5,8 @@ app_name = 'remote'
 
 urlpatterns = [
     path('', views.Index.as_view(), name = "index"),
-    path('capture', views.Capture.as_view(), name = "capture"),
+    path('<album>/capture', views.Capture.as_view(), name = "capture"),
+    path('<album>', views.AlbumView.as_view(), name = "album"),
+    path('<album>/<int:number>', views.PhotoView.as_view(), name = "photo"),    
     path('occupied', views.Occupied.as_view(), name = "occupied"),
 ]
--- remote/views.py
+++ remote/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+from django.http import Http404
 
-from django.views.generic import TemplateView
-from .models import CameraStatus
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import CameraStatus, Album, Photo
 import os, subprocess
 from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SHOW_FULL_SIZE_IMAGE_ON_CAPTURE = True
 
 class Index(TemplateView):
     template_name = "remote/index.html"    
@@ -21,9 +23,8 @@ def get_last_image_link(folder_name):
     image_count = int(f.readlines()[0])
     f.close()
     os.chdir(BASE_DIR)
-    image_path = "/media/" + folder_name
-    make_resized_image(image_path + "/bilde_" + str(image_count) + ".JPG");
-    return image_path + "/bilde_" + str(image_count) + ".JPG"
+    make_resized_image(folder_name + "/bilde_" + str(image_count) + ".JPG");
+    return folder_name + "/bilde_" + str(image_count) + ".JPG"
 
 def get_or_create_camera_status():
     if not CameraStatus.objects.exists():
@@ -35,19 +36,59 @@ class Capture(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Capture, self).get_context_data( **kwargs)
-        context["image_link"] = get_last_image_link("remote_images")
+        photo = Photo.objects.all().last()
+        if (SHOW_FULL_SIZE_IMAGE_ON_CAPTURE):
+            context["image_link"] = photo.image.url
+        else:
+            context["image_link"] = photo.image_lowres.url
+        context["album"] = self.kwargs["album"]
         return context
     
     def get(self, request, *args, **kwargs):
+        album = get_object_or_404(Album, name=self.kwargs["album"])
+        
         status = get_or_create_camera_status()
         if (status.occupied):
             return redirect("remote:occupied")
         status.occupied=True;
         status.save()
-        subprocess.call(["python3", "remote/image_capture.py", "remote_images"])
+        subprocess.call(["python3", "remote/image_capture.py", album.name])
         status.occupied=False;
         status.save()
+        photo = Photo()
+        photo.album = album
+        photo.image.name=get_last_image_link(album.name)
+        photo.save()
         return super(Capture, self).get(request, *args, **kwargs)
 
+class AlbumView(ListView):
+    template_name = "remote/album.html"
+    context_object_name = "photos"
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumView, self).get_context_data( **kwargs)
+        context["album"] = self.kwargs["album"]
+        return context
+    
+    def get_queryset(self):
+        album = get_object_or_404(Album, name=self.kwargs["album"])
+        return Photo.objects.filter(album=album).order_by('-shot_time')
+
+class PhotoView(TemplateView):
+    template_name = "remote/photo.html"
+    context_object_name = "photo"
+    
+    def get_context_data(self, **kwargs):
+        album = get_object_or_404(Album, name=self.kwargs["album"])
+        try:
+            photo = Photo.objects.filter(album=album).order_by('-shot_time')[self.kwargs["number"]-1]
+        except:
+            raise Http404
+        context = super(PhotoView, self).get_context_data( **kwargs)
+        context["photo"] = photo
+        context["album"] = self.kwargs["album"]
+        return context
+    
 class Occupied(TemplateView):
     template_name = "remote/occupied.html"
