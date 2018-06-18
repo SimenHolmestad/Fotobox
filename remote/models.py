9a4f65b84acf7552e1d14c48b64e0ffe7bbc7919
SimenHolmestad <simen@holmestad.com>
2018-06-18 10:38:45 +0000

    Added functionality for storing images in django-models

--- remote/models.py
+++ remote/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from datetime import datetime
+from django.core.files import File
+from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
+from PIL import Image
+import os
+from io import BytesIO
 
 # Create your models here.
+
+THUMBNAIL_SIZE = 1024, 1024
 
 class CameraStatus (models.Model):
     occupied = models.BooleanField(default=False)
@@ -12,5 +21,47 @@ class CameraStatus (models.Model):
             # then error will also raised in update of exists model
             raise ValidationError("There can only be one cameraStatus instance")
         return super(CameraStatus, self).save(*args, **kwargs)
-            
+
+def create_folder(folder_location):
+    try:
+        os.makedirs(folder_location)
+    except:
+        print(folder_location + " already in place")
+    
+class Photo (models.Model):
+    shot_time = models.DateTimeField(default=datetime.now)
+    image = models.ImageField()
+    image_lowres = models.ImageField()
+    album = models.ForeignKey("Album", on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.make_thumbnail():
+            # set to a default thumbnail
+            raise Exception('Could not create thumbnail - is the file type valid?')
+        super(Photo, self).save(*args, **kwargs)
+
+    def make_thumbnail(self):
+        image = Image.open(self.image.path)
+        image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+        image_filename = self.image.url.split("/")[-1]
+        image_folder = "/".join(self.image.path.split("/")[0:-1])
+        create_folder(image_folder + "/lowres")
+        rel_image_folder = "/".join(self.image.url.split("/")[2:-1]) # we have to omit media, therefore the [2:-1]
+
+        thumb_filename = rel_image_folder + "/lowres/" + image_filename.split(".")[0] + "_lowres.JPG"
+
+        # Save thumbnail to in-memory file as StringIO
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, "JPEG")
+        temp_thumb.seek(0)
         
+        # set save=False, otherwise it will run in an infinite loop
+        print ("thumf_filename: " + thumb_filename)
+        self.image_lowres.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+        return True
+
+class Album (models.Model):
+    time_made = models.DateTimeField(default=datetime.now)
+    name = models.CharField(max_length = 30)
--- remote/tests.py
+++ remote/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.core.files import File
+from remote.models import Album, Photo
+from PIL import Image
+import os
+from io import BytesIO
 
-# Create your tests here.
+class image_save_test(TestCase):
+    def test_save_image(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dir_path = "/".join(dir_path.split("/")[0:-1])
+        print (dir_path)
+        photo = Photo()
+        photo.album = Album.objects.create(name = "test_album")
+        photo.image.name = "remote_images/bilde_1.JPG"        
+        photo.save()
